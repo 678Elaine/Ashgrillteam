c3dfb4c8111268253363144c35b40c4988b7f2b7
OgechiNwankwo <95922876+OgechiNwankwo@users.noreply.github.com>
2022-11-23 12:38:32 +0000

    Update register.php

--- View/register.php
+++ View/register.php
@@ -129,21 +129,21 @@
                                         </div>
 
                                         <div class="form-outline mb-4">
-                                            <label class="form-label" for="formTelephone">Telephone Number</label>
+                                            <label class="form-label" for="formCountry">Telephone Number</label>
                                             <input type="number" id="formCountry" name="number" class="form-control"
-                                                placeholder="Country" />
+                                                placeholder="Number" />
                                         </div>
 
                                         <div class="form-outline mb-4">
                                             <label class="form-label" for="formCity">Business Name</label>
                                             <input type="text" id="formCity" name="business_name" class="form-control"
-                                                placeholder="City" />
+                                                placeholder="Business Name" />
                                         </div>
 
                                         <div class="form-outline mb-4">
                                             <label class="form-label" for="formContact">Student ID</label>
                                             <input type="text" id="formContact" name="student_id" class="form-control"
-                                                placeholder="Contact" />
+                                                placeholder="Student ID" />
                                         </div>
 
                                         </div>
