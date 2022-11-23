aa096d506f5161a98e1a5473158e5bc7069d087b
RitaSegbaya <94528508+RitaSegbaya@users.noreply.github.com>
2022-11-23 13:32:25 +0000

    Update booking.php

--- View/booking.php
+++ View/booking.php
@@ -25,15 +25,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="View1/lib/animate/animate.min.css" rel="stylesheet">
-    <link href="View1/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="View1/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="View1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="View1/css/style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 
     <script src="https://js.paystack.co/v1/inline.js"></script> 
     
