0f835c2d4327f5b61fab6172cb6e23145f877d78
678Elaine <94685092+678Elaine@users.noreply.github.com>
2022-11-24 02:17:30 +0000

    Update db_cred.php

--- Settings/db_cred.php
+++ Settings/db_cred.php
@@ -1,8 +1,8 @@
 <?php
 
-define("DB_NAME", "ashgrill");
-define("DB_SERVER", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
+define("DB_NAME", "heroku_a9cf8ff87408bef");
+define("DB_SERVER", "us-cdbr-east-06.cleardb.net");
+define("DB_USERNAME", "bd5c90a8ea3f32");
+define("DB_PASSWORD", "fa62a309");
 
 ?> 
